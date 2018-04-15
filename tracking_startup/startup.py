6e97647a5b42ad5f07685dc3bc7d97914abb28e4
Sebastian Gustafsson <carlsebastiangustafsson@gmail.com>
2018-04-15 19:09:12 +0200

    beginning of tracking script

--- tracking_startup/startup.py
+++ tracking_startup/startup.py
@@ -1,2 +1,42 @@
+#################################################################
+# Python Script for sending connection information from client to SQL-database
+# Authors: Albin Hjelm, Alexander Backlund & Sebastian Gustafsson
+#
+# Script Idea:
+# 1. Check wether it's run on an Windows or Linux machine
+# 2. Prompt for user input, UserID or such
+# 3. Scan with wifi-card for available AccessPoints Limit to 5 results
+# 4. Connect to SQL-database and insert data to table
+# 5. Open website. (Which also might be inserted through userinput)
+#
+#################################################################
+#Todos:
+# TODO: prompting user for  checking which networkcard they want to use
+# TODO: (maybe) prompt user for which system they're on
+# TODO: prompt user for username
+#################################################################
+
 from wifi import Cell, Scheme
-Cell.all('wlan0')
+import time, platform
+
+def checkOperatingSys():
+    return platform.system()
+
+#def checkWifiCard():
+
+def scanNetworks(netwrkCard):
+    cells = Cell.all(netwrkCard)
+    while(len(cells) < 2):
+        cells = Cell.all(netwrkCard)
+        time.sleep(2)
+        print('Scanning...')
+    for c in cells:
+        print(c.ssid+" - "+str(c.signal)+" - "+str(c.address))
+
+def main():
+    netcard = raw_input("Enter network-card for wifi-scan: ")
+
+    scanNetworks(netcard)
+
+if __name__ == "__main__":
+    main()
