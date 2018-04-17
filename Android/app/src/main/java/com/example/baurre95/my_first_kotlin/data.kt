b5dcf338b209a50ccc8e49cb2e49e4c942d9a1e1
baurre95 <alexanderbacklund95@gmail.com>
2018-04-17 16:20:41 +0200

    Albins order

--- Android/app/src/main/java/com/example/baurre95/my_first_kotlin/data.kt
+++ Android/app/src/main/java/com/example/baurre95/my_first_kotlin/data.kt
@@ -1,12 +1,22 @@
 package com.example.baurre95.my_first_kotlin
 
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.wifi.ScanResult
+import android.net.wifi.WifiManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.PopupMenu
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_calibration.*
 import kotlinx.android.synthetic.main.activity_data.*
 import java.io.File
 
@@ -14,6 +24,17 @@ class data : AppCompatActivity() {
     val FILENAME = "accesspoints.txt"
     var myList =  listOf<String>()
 
+    val REQUEST_FINE_LOCATION: Int=0
+    var resultList = ArrayList<ScanResult>()
+    val axisList = ArrayList<String>()
+    lateinit var wifiManager: WifiManager
+
+    val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(contxt: Context?, intent: Intent?) {
+            resultList = wifiManager.scanResults as ArrayList<ScanResult>
+            Log.d("TESTING", "onReceive Called")
+        }
+    }
 
     fun printFromFile(file: File) {
         myList = file.readLines()
@@ -25,6 +46,66 @@ class data : AppCompatActivity() {
     fun writeToFile(file: File,text: String) {
         file.appendText(text)
 
+    }
+    fun startScanning(){
+        if (resultList.size > 0 || axisList.size > 0) {
+            resultList.clear()
+            axisList.clear()
+        }
+
+        wifiManager.setWifiEnabled(true)
+        registerReceiver(broadcastReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+        val enabled: Boolean = wifiManager.isWifiEnabled()
+        mayRequestLocation()
+        wifiManager.startScan()
+
+        Handler().postDelayed({
+            this.stopScanning()
+        }, 5000)
+    }
+
+    fun stopScanning() {
+        unregisterReceiver(broadcastReceiver)
+        try {
+            resultList.sortedWith(compareBy({ it.level }))
+            for (i in 0..resultList.size) {
+                axisList.add(resultList[i].SSID+" "+resultList[i].BSSID+" "+resultList[i].level)
+                if(i == 10) {
+                    break
+                }
+            }
+
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, axisList)
+            listView_bssid.adapter = adapter
+            Log.d("TESTING", axisList.toString())
+
+        } catch (e: IndexOutOfBoundsException) {
+            Toast.makeText(this, "Scan Failed", Toast.LENGTH_SHORT).show()
+
+        }
+
+
+    }
+    private fun mayRequestLocation(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true
+        }
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true
+        }
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_FINE_LOCATION)
+
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_FINE_LOCATION)
+        }
+        return false
+    }
+
+
+    fun scan_rp() {
+        wifiManager = this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        startScanning()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,6 +132,7 @@ class data : AppCompatActivity() {
             popupMenu.setOnMenuItemClickListener { item  ->
                 when (item.itemId) {
                     R.id.menu_start_scan -> {
+                        //return scan_rp()
                         val intent = Intent(this, calibration::class.java)
                         startActivity(intent)
                         true
