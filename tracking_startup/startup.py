8099ceacbed123f3e11cacf8948a7d12a267f944
Sebastian Gustafsson <carlsebastiangustafsson@gmail.com>
2018-04-17 13:46:38 +0200

    began with mysql

--- tracking_startup/startup.py
+++ tracking_startup/startup.py
@@ -17,12 +17,13 @@
 #################################################################
 
 from wifi import Cell, Scheme
-import time, platform
+import time, platform, MySQLdb
+
+def connectToDB():
+    return MySQLdb.connect(host="back.db1.course.it.uu.se", user="fall17_it12", passwd="vXdWAk2K", db="fall17_project_it12")
 
 def checkOperatingSys():
     return platform.system()
-
-#def checkWifiCard():
 
 def scanNetworks(netwrkCard):
     cells = Cell.all(netwrkCard)
@@ -36,13 +37,29 @@ def sortNetworksReturnAmount(amountReturned, netwrkList):
     nList = sorted(netwrkList, key=lambda x: x.signal, reverse=True)[:amountReturned]
     return nList
 
-def main():
-    netcard = raw_input("Enter network-card for wifi-scan: ")
+def getRefListFromDB(cur):
+    cur.execute("SELECT * FROM SEBBE") # TODO: Change to reftable
+    return cur.fetchall()
 
-    l = scanNetworks(netcard)
-    l1 = sortNetworksReturnAmount(4,l)
-    for l2 in l1:
-        print(l2.ssid+" - "+str(l2.signal)+" - "+str(l2.address))
+def main():
+    #netcard = raw_input("Enter network-card for wifi-scan: ")
+
+    # l = scanNetworks(netcard)
+    # l1 = sortNetworksReturnAmount(4,l)
+    db = connectToDB()
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor()
+
+    getRefListFromDB(cur)
+
+
+
+
+
+    #Leave me be
+    ###############################################
+    db.close()
 
 if __name__ == "__main__":
     main()
