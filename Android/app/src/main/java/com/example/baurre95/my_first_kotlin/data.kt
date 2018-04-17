615f327240837ac7ecad86b3e408ea75be981af7
baurre95 <alexanderbacklund95@gmail.com>
2018-04-17 18:15:43 +0200

    Fortsatte arbeta lite

--- Android/app/src/main/java/com/example/baurre95/my_first_kotlin/data.kt
+++ Android/app/src/main/java/com/example/baurre95/my_first_kotlin/data.kt
@@ -16,12 +16,13 @@ import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.PopupMenu
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_calibration.*
 import kotlinx.android.synthetic.main.activity_data.*
 import java.io.File
 
 class data : AppCompatActivity() {
-    val FILENAME = "accesspoints.txt"
+    val filenameToDB = "accesspointsToDb.csv"
+    val filenameToUsers = "ReferencePointsToUser.csv"
+
     var myList =  listOf<String>()
 
     val REQUEST_FINE_LOCATION: Int=0
@@ -38,7 +39,6 @@ class data : AppCompatActivity() {
 
     fun printFromFile(file: File) {
         myList = file.readLines()
-        Log.d("TESTING", myList.toString())
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, myList)
         APlv.adapter = adapter
     }
@@ -47,7 +47,7 @@ class data : AppCompatActivity() {
         file.appendText(text)
 
     }
-    fun startScanning(){
+    fun startScanning(file: File){
         if (resultList.size > 0 || axisList.size > 0) {
             resultList.clear()
             axisList.clear()
@@ -60,11 +60,11 @@ class data : AppCompatActivity() {
         wifiManager.startScan()
 
         Handler().postDelayed({
-            this.stopScanning()
+            this.stopScanning(file)
         }, 5000)
     }
 
-    fun stopScanning() {
+    fun stopScanning(file: File) {
         unregisterReceiver(broadcastReceiver)
         try {
             resultList.sortedWith(compareBy({ it.level }))
@@ -74,11 +74,11 @@ class data : AppCompatActivity() {
                     break
                 }
             }
-
-            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, axisList)
-            listView_bssid.adapter = adapter
-            Log.d("TESTING", axisList.toString())
-
+            var textToDB = ""
+            for (i in 0..axisList.size) {
+                textToDB +="\n" + axisList[i]
+                writeToFile(file, textToDB)
+            }
         } catch (e: IndexOutOfBoundsException) {
             Toast.makeText(this, "Scan Failed", Toast.LENGTH_SHORT).show()
 
@@ -103,38 +103,45 @@ class data : AppCompatActivity() {
     }
 
 
-    fun scan_rp() {
-        wifiManager = this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        startScanning()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data)
+
+        wifiManager = this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
         val path = getExternalFilesDir(null)
+        val filecheckDB = File(path.toString() +  "/" + "Saved Files"+ "/" + filenameToDB)
+        val filecheckUser = File(path.toString() +  "/" + "Saved Files"+ "/" + filenameToUsers)
 
-        val filecheck = File(path.toString() +  "/" + "Saved Files"+ "/" + FILENAME)
-
-        if (filecheck.exists()) {
-            Toast.makeText(this, "File exists", Toast.LENGTH_SHORT).show()
-
+        if (filecheckDB.exists()) {
+            Toast.makeText(this, "File DB exists", Toast.LENGTH_SHORT).show()
         }
         else {
             val letDirectory = File(path,"Saved Files")
             letDirectory.mkdirs()
-            val file = File(letDirectory, FILENAME)
+            val file = File(letDirectory, filenameToDB)
             writeToFile(file, "a")
         }
-        printFromFile(filecheck)
+
+        if (filecheckUser.exists()) {
+            Toast.makeText(this, "File user exists", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            val letDirectory = File(path,"Saved Files")
+            letDirectory.mkdirs()
+            val file2 = File(letDirectory, filenameToUsers)
+            writeToFile(file2, "a")
+        }
+
+        printFromFile(filecheckUser)
 
         APlv.setOnItemClickListener {parent, view, position, id ->
             val popupMenu = PopupMenu(this, view)
             popupMenu.setOnMenuItemClickListener { item  ->
                 when (item.itemId) {
                     R.id.menu_start_scan -> {
-                        //return scan_rp()
-                        val intent = Intent(this, calibration::class.java)
-                        startActivity(intent)
+                        startScanning(filecheckDB)
+                        Toast.makeText(this, "Klart", Toast.LENGTH_LONG).show()
                         true
                     }
                     R.id.menu_delete -> {
@@ -154,9 +161,10 @@ class data : AppCompatActivity() {
         APaddbtn.setOnClickListener{
             var textFromET = APet.text.toString()
             textFromET += "\n"
-            writeToFile(filecheck, textFromET)
+            writeToFile(filecheckUser, textFromET)
+            writeToFile(filecheckDB, textFromET)
 
-            printFromFile(filecheck)
+            printFromFile(filecheckUser)
 
         }
     }
