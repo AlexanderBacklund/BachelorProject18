561facc065bcb2cd88f509716ccfeab9e5918e5f
baurre95 <alexanderbacklund95@gmail.com>
2018-04-25 15:52:52 +0200

    kan visa vart folket är

--- django-ui/localisation_ui/show_position/views.py
+++ django-ui/localisation_ui/show_position/views.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from show_position.models import refpoint, user_position
 #from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    funny_dict = {'msg': 'Albin kan allt',}
+    users = user_position.objects.all()
+    funny_dict = {'user_positions': users,}
     return render(request, 'show_position/index.html', funny_dict)
